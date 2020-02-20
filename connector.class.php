e28e4ae3874a7793bc3cf10e0e6d156db991d893
ricardopxl <rebocano@gmail.com>
2020-02-20 13:00:39 -0300

    First commit
    Connector class and search demo file.

---
+++ connector.class.php
@@ -0,0 +1,49 @@
+<?php  
+	class Search 
+	{
+		public $endPoint = 'http://url/api/';
+		public $authorization = "YOUR_TOKEN_AUTHORIZATION";
+		public $outPut = array('output_format'=>'JSON'); # RECOMENDED FOR THIS CODE
+		
+		public function find($action=null,$params)
+		{
+			$params = http_build_query(array_merge($params,$this->outPut));
+			$url = $this->endPoint.$action."?".$params;
+			return $this->getCurl($url);
+			// return $this->getCurl($this->endPointurl,$action."?".$params);
+		}
+
+		private function getCurl($url,$event='')
+		{
+			$ch = curl_init();
+			$url = $url.$event;
+			curl_setopt($ch, CURLOPT_USERPWD, $this->authorization.":");
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$resp = curl_exec($ch);
+			curl_close($ch);
+
+			return $resp;
+		}
+
+		private function postCurl($url, $event = "", $json = "")
+		{
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,$url.$event);
+			
+			if (!empty($json)) 
+			{
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($json));
+			}
+
+			$authorization = "token: ".$this->token;
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array($authorization));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$server_output = curl_exec($ch);
+			curl_close ($ch);
+
+			return $server_output;
+		}
+	}
+?>
---
+++ search-demo.php
@@ -0,0 +1,60 @@
+<?php 
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	set_time_limit(0);
+	header('Content-Type: application/json');
+	
+	require('connector.class.php');
+
+	if (!isset($_GET['query'])) {
+		$arreglo = array('status'=>false,'data'=>'not_query_param');
+		die(json_encode($arreglo));	
+	}
+
+	/**
+	* GET DATA, please, sanitize the next query.
+	**/
+	$query = $_GET['query'];
+	
+	try {
+
+		/**
+		* Configuration available for this code
+		**/
+		$search = new Search();
+		$search->endPoint = 'https://mywebsite.cl/api/';
+		$search->authorization = 'MYTOKENASDFGHJKLJHGFD';
+		
+		/*BULD PARAMS TO SEARCH*/
+		$params = array(
+					'language' 	=> 1,
+					'query' 	=> $query,
+					'limit'		=> '0,100',
+					'display'	=> 'full'
+				);
+
+		$response = $search->find('search',$params);
+		$response = json_decode($response);
+
+		/**
+		* Validate if responses has product
+		**/
+		if (!isset($response->products) AND empty($response->products)) {
+			$arreglo = array('status'=>false,'data'=>'empty');
+			die(json_encode($arreglo));
+		}
+
+		$arreglo = array('status'=>true,'data'=>$response->products);
+		die(json_encode($arreglo));
+
+	} catch (Exception $e) {
+		if (!isset($response->products) AND empty($response->products)) {
+			$arreglo = array('status'=>false,'data'=>'empty');
+			die(json_encode($arreglo));
+		}		
+	}
+?>
